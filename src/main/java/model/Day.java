c395c4b08be43b9c18c1aeca0ffece4261b06846
yeojin <ahnyeojin256@gmail.com>
2024-11-30 00:29:04 +0900

    feat: 모임 날짜 관련 클래스 추가

---
+++ src/main/java/model/Day.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Day {
+	private String day;
+	private String startTime;
+	private String endTime;
+	
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+}
